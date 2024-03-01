fdb1f7084ed48ead0ab3048305331a0c3ee0d43e
Simple <huyang1994822@gmail.com>
2024-03-01 22:14:10 +0800

    udpate

--- novel.py
+++ novel.py
@@ -5,12 +5,14 @@ import requests
 from bs4 import BeautifulSoup
 
 next_re = re.compile("（翻页提示.*）")
+file_name_re = re.compile("\\d{2}")
 
 
 def main():
     # 目录的网址
     # baseurl = "https://www.daomubiji.com/dao-mu-bi-ji-1"
     article_link = "https://www.daomubiji.com/qi-xing-lu-wang-01.html"
+    sub_dir = '1-qi-xing-lu-wang-gong'
     while len(article_link) != 0:
         head = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
@@ -20,14 +22,19 @@ def main():
         req.encoding = 'UTF-8'
         soup = BeautifulSoup(req.text, 'html.parser')
         content = soup.find('div', class_="content", attrs={"class": "content"})
-        print(content)
+        # print(content)
         title_split = content.find('h1', class_='article-title').text.split(' ')
         title = title_split[1] + ' ' + title_split[2]
-        print(title)
+        # print(title)
         article = content.find('article', class_="article-content").text
         article = next_re.sub('', article)
         article = article.replace('\n', '\n\n')
-        print(article)
+        # print(article)
+        file_name = file_name_re.findall(article_link)[0]
+        with open(sub_dir + '/' + file_name + '.md', 'w', encoding='UTF-8', newline='\n') as f:
+            f.write("# " + title + '\n')
+            f.write(article)
+
         article_link = content.find('a', attrs={"rel": "next"}).attrs['href']
         print(article_link)
 
