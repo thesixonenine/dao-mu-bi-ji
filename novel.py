a62fa3259812b5e03075be6ec79539eca643bc42
Simple <huyang1994822@gmail.com>
2024-03-03 13:37:49 +0800

    update

--- novel.py
+++ novel.py
@@ -1,6 +1,7 @@
 import re
 import time
 
+import cn2an
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,12 +10,10 @@ file_name_re = re.compile("\\d{2}")
 
 
 def main():
-    # 目录的网址
-    # baseurl = "https://www.daomubiji.com/dao-mu-bi-ji-1"
-    base_name = 'qiong-long-shi-ying'
-    article_link = "https://www.daomubiji.com/" + base_name + "-01.html"
-    sub_dir = '9-' + base_name
-    while len(article_link) != 0 and article_link.startswith('https://www.daomubiji.com/' + base_name):
+    article_link = "https://www.daomubiji.com/dajieju-0101.html"
+    sub_dir = '10-da-jie-ju'
+    i = 1
+    while len(article_link) != 0:
         head = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
         time.sleep(1)
@@ -25,13 +24,15 @@ def main():
         content = soup.find('div', class_="content", attrs={"class": "content"})
         # print(content)
         title_split = content.find('h1', class_='article-title').text.split(' ')
-        title = title_split[1] + ' ' + title_split[2]
+        cp_code = str(i)
+        cp_code_cn = '第' + cn2an.an2cn(cp_code) + '章'
+        title = cp_code_cn + ' ' + title_split[2]
         # print(title)
         article = content.find('article', class_="article-content").text
         article = next_re.sub('', article)
         article = article.replace('\n', '\n\n')
         # print(article)
-        file_name = file_name_re.findall(article_link)[0]
+        file_name = '%02d' % i
         # file_name = str(int(file_name) + 75)
         with open(sub_dir + '/' + file_name + '.md', 'w', encoding='UTF-8', newline='\n') as f:
             f.write("# " + title + '\n')
@@ -40,6 +41,7 @@ def main():
             f.write("  * [" + title + '](' + sub_dir + '/' + file_name + '.md)\n')
         article_link = content.find('a', attrs={"rel": "next"}).attrs['href']
         print(article_link)
+        i = i + 1
 
 
 if __name__ == "__main__":
